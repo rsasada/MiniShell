22bb478e556ee758b7f2515319a6eab92889c7c3
jongyoon kim <johntripla@jongyoon-kim.local>
2024-02-25 23:35:52 +0900

    fix norm

--- srcs/builtin/ft_export.c
+++ srcs/builtin/ft_export.c
@@ -31,35 +31,55 @@ static int	is_valid_arg(char *arg)
 	return (1);
 }
 
-void	ft_export(t_app *app, t_list *argv, pid_t *pid)
+static int	validate_pid(t_app *app, pid_t *pid)
 {
 	pid_t	*first_pid;
-	char	*first_arg;
-	t_list	*argv_cur;
-	char	*arg;
 
 	if (!app || !app->pid_storage || !pid)
-		return ;
-	first_pid = (pid_t *) (app->pid_storage->content);
+		return (0);
+	first_pid = (pid_t *)(app->pid_storage->content);
 	if (pid != first_pid)
-		return ;
+		return (0);
+	return (1);
+}
+
+static int	process_first_arg(t_app *app, t_list *argv)
+{
+	char	*first_arg;
+
 	first_arg = ((t_ast_node *) argv->content)->u_node_data.file_name_val;
 	if (first_arg || *first_arg == '#')
 	{
 		print_env(app, 1);
-		return ;
+		return (0);
 	}
+	return (1);
+}
+
+static void	process_args(t_list *argv)
+{
+	t_list	*argv_cur;
+	char	*arg;
+
 	argv_cur = argv;
 	while (argv_cur)
 	{
-		arg = ((t_ast_node *) (argv_cur->content))->u_node_data.file_name_val;
+		arg = ((t_ast_node *)(argv_cur->content))->u_node_data.file_name_val;
 		if (!is_valid_arg(arg))
-			if (!is_valid_arg(arg))
-			{
-				ft_putstr_fd("push: export: `", STDERR_FILENO);
-				ft_putstr_fd(arg, STDERR_FILENO);
-				handling_error("': not a valid identifier", 1);
-			}
+		{
+			ft_putstr_fd("push: export: `", STDERR_FILENO);
+			ft_putstr_fd(arg, STDERR_FILENO);
+			handling_error("': not a valid identifier", 1);
+		}
 		argv_cur = argv_cur->next;
 	}
+}
+
+void	ft_export(t_app *app, t_list *argv, pid_t *pid)
+{
+	if (!validate_pid(app, pid))
+		return ;
+	if (!process_first_arg(app, argv))
+		return ;
+	process_args(argv);
 }
