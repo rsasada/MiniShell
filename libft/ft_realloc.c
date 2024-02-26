f2656019a1506d11cb5401e7b2ff7349a37e4164
jongyoon kim <johntripla@jongyoon-kim.local>
2024-02-27 01:55:24 +0900

    reimplement realloc

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -12,24 +12,21 @@
 
 #include "libft.h"
 
-void *ft_realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t size)
 {
-    char *src;
-    char *dst;
-    int i;
+	void	*new_ptr;
 
-    src = (char *) ptr;
-    dst = (char *) malloc(size);
-    if (!dst) {
-        return (NULL);
-    }
-    if (src != NULL) {
-        i = 0;
-        while (src[i]) {
-            dst[i] = src[i];
-            i++;
-        }
-    }
-    free(ptr);
-    return ((void *) dst);
+	if (size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (malloc(1));
+	}
+	if (!ptr)
+		return (malloc(size));
+	new_ptr = malloc(size);
+	if (!new_ptr)
+		return (NULL);
+	ft_memcpy(new_ptr, ptr, size);
+	free(ptr);
+	return (new_ptr);
 }
