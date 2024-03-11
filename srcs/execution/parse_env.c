124a6ab9b8a5dc917bd9b133c96d02f5e4105a47
jongyoon kim <johntripla@jongyoon-kim.local>
2024-03-11 20:20:31 +0900

    fix wrong name

--- srcs/execution/parse_env.c
+++ srcs/execution/parse_env.c
@@ -64,13 +64,13 @@ void	exit_access_error(int error_code, char *path)
 {
 	if (error_code == F_OK)
 	{
-		ft_putstr_fd("bash: ", STDERR_FILENO);
+		ft_putstr_fd("push: ", STDERR_FILENO);
 		ft_putstr_fd(path, STDERR_FILENO);
 		ft_putendl_fd(": command not found ", STDERR_FILENO);
 	}
 	else if (error_code == X_OK)
 	{
-		ft_putstr_fd("bash: ", STDERR_FILENO);
+		ft_putstr_fd("push: ", STDERR_FILENO);
 		ft_putstr_fd(path, STDERR_FILENO);
 		ft_putendl_fd(": Pemission denied ", STDERR_FILENO);
 	}
